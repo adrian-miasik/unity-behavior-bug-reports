209f5dabad48ebab8fda43e357fc14bc5be3fc78
Adrian Miasik <git@adrian-miasik.com>
2024-10-11 19:18:15 -0400

    Create reproducible "Failed to construct type" error using new action -> Implemented into 'serialization-type-construct-fail-case-unity' scene

---
+++ UnityBugBehaviorTypes/Assets/Samples/Behavior/1.0.2/Runtime Serialization/Additional Scripts/AgentNavigatesToLocationWhileLookingAtQueueSlotAction.cs
@@ -0,0 +1,146 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using UnityEngine.AI;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Agent navigates to location while looking at QueueSlot",
+    story: "[Agent] navigates to [location] while looking at [QueueSlot]", category: "Action",
+    id: "955bce537bc45f0af9e504726b1c3d13")]
+public partial class AgentNavigatesToLocationWhileLookingAtQueueSlotAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    [SerializeReference] public BlackboardVariable<Vector3> Location;
+    [SerializeReference] public BlackboardVariable<float> Speed = new(1.0f);
+    [SerializeReference] public BlackboardVariable<float> DistanceThreshold = new(0.2f);
+
+    [SerializeReference]
+    public BlackboardVariable<string> AnimatorSpeedParam = new("SpeedMagnitude");
+
+    [SerializeReference] public BlackboardVariable<QueueSlot> QueueSlot;
+
+    // This will only be used in movement without a navigation agent.
+    [SerializeReference] public BlackboardVariable<float> SlowDownDistance = new(1.0f);
+
+    private float m_PreviousStoppingDistance;
+    private NavMeshAgent m_NavMeshAgent;
+    private Animator m_Animator;
+
+    protected override Status OnStart()
+    {
+        if (ReferenceEquals(Agent?.Value, null) || ReferenceEquals(Location?.Value, null))
+        {
+            return Status.Failure;
+        }
+
+        return Initialize();
+    }
+
+    protected override Status OnUpdate()
+    {
+        if (ReferenceEquals(Agent?.Value, null) || ReferenceEquals(Location, null))
+        {
+            return Status.Failure;
+        }
+
+        if (m_NavMeshAgent == null)
+        {
+            Vector3 agentPosition, locationPosition;
+            float distance = GetDistanceToLocation(out agentPosition, out locationPosition);
+            if (distance <= DistanceThreshold)
+            {
+                return Status.Success;
+            }
+
+            float speed = Speed;
+
+            if (SlowDownDistance > 0.0f && distance < SlowDownDistance)
+            {
+                float ratio = distance / SlowDownDistance;
+                speed = Mathf.Max(0.1f, Speed * ratio);
+            }
+
+            Vector3 toDestination = locationPosition - agentPosition;
+            toDestination.y = 0.0f;
+            toDestination.Normalize();
+            agentPosition += toDestination * (speed * Time.deltaTime);
+            Agent.Value.transform.position = agentPosition;
+
+            // Look at the QueueSlot
+            Agent.Value.transform.LookAt(QueueSlot.Value.transform.position);
+        }
+        else if (!m_NavMeshAgent.pathPending && m_NavMeshAgent.remainingDistance <= m_NavMeshAgent.stoppingDistance)
+        {
+            return Status.Success;
+        }
+
+        return Status.Running;
+    }
+    
+    protected override void OnEnd()
+    {
+        if (m_Animator != null)
+        {
+            m_Animator.SetFloat(AnimatorSpeedParam, 0);
+        }
+
+        if (m_NavMeshAgent != null)
+        {
+            if (m_NavMeshAgent.isOnNavMesh)
+            {
+                m_NavMeshAgent.ResetPath();
+            }
+
+            m_NavMeshAgent.stoppingDistance = m_PreviousStoppingDistance;
+        }
+
+        m_NavMeshAgent = null;
+        m_Animator = null;
+    }
+
+    protected override void OnDeserialize()
+    {
+        Initialize();
+    }
+
+    private Status Initialize()
+    {
+        if (GetDistanceToLocation(out Vector3 agentPosition, out Vector3 locationPosition) <= DistanceThreshold)
+        {
+            return Status.Failure;
+        }
+
+        // If using animator, set speed parameter.
+        m_Animator = Agent.Value.GetComponentInChildren<Animator>();
+        if (m_Animator != null)
+        {
+            m_Animator.SetFloat(AnimatorSpeedParam, Speed);
+        }
+
+        // If using a navigation mesh, set target position for navigation mesh agent.
+        m_NavMeshAgent = Agent.Value.GetComponentInChildren<NavMeshAgent>();
+        if (m_NavMeshAgent != null)
+        {
+            if (m_NavMeshAgent.isOnNavMesh)
+            {
+                m_NavMeshAgent.ResetPath();
+            }
+
+            m_NavMeshAgent.speed = Speed;
+            m_PreviousStoppingDistance = m_NavMeshAgent.stoppingDistance;
+            m_NavMeshAgent.stoppingDistance = DistanceThreshold;
+            m_NavMeshAgent.SetDestination(locationPosition);
+        }
+
+        return Status.Running;
+    }
+
+    private float GetDistanceToLocation(out Vector3 agentPosition, out Vector3 locationPosition)
+    {
+        agentPosition = Agent.Value.transform.position;
+        locationPosition = Location.Value;
+        return Vector3.Distance(new Vector3(agentPosition.x, locationPosition.y, agentPosition.z), locationPosition);
+    }
+}
